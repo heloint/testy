b78f756c3a3f3d6540682468b9a78ea237f5568e
heloint <majerdaniel93@gmail.com>
2025-02-04 15:00:46 +0100

    tbc

---
+++ _scripts/github_actions_deploy.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import paramiko
+import argparse
+from typing import TypedDict
+from dataclasses import dataclass
+
+class SshCmdResult(TypedDict):
+    resultcode: int
+    stdout: str
+    stderr: str
+
+class SshUtils:
+    @classmethod
+    def get_ssh_client(
+        cls, server: str, username: str, ssh_key_file: str
+    ) -> paramiko.SSHClient:
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(server, username=username, key_filename=ssh_key_file)
+        return ssh_client
+
+    @classmethod
+    def execute_command_on_remote(
+        cls, ssh_client: paramiko.SSHClient, command
+    ) -> SshCmdResult:
+        _, stdout, stderr = ssh_client.exec_command(command)
+        return_code = stdout.channel.recv_exit_status()
+        stdout_output = stdout.read().decode().strip()
+        stderr_output = stderr.read().decode().strip()
+        return {
+            "resultcode": return_code,
+            "stdout": stdout_output,
+            "stderr": stderr_output,
+        }
+
+
+@dataclass
+class GithubActionsDeployArgs:
+    server_address: str
+    username: str
+    ssh_key_file: str
+
+    @classmethod
+    def get_arguments(cls) ->  GithubActionsDeployArgs:
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawTextHelpFormatter,
+            description="""
+    Runs healthcheck on the bsccgenomics02 virtual machine, checking the running docker containers.
+    """,
+        )
+        parser.add_argument(
+            "--server_address",
+            help="Remote address of the target server.",
+            type=str,
+            required=True,
+        )
+        parser.add_argument(
+            "--username",
+            help="Username on the target server.",
+            type=str,
+            required=True,
+        )
+        parser.add_argument(
+            "--ssh_key_file",
+            help="Password on the target server.",
+            type=str,
+            required=True,
+        )
+        return GithubActionsDeployArgs(**vars(parser.parse_args()))
+
+def run_deployment(ssh_client: paramiko.SSHClient) -> None:
+    res: SshCmdResult = SshUtils.execute_command_on_remote(
+        ssh_client, "ls -lisah /var/www/web/phylomedb6-reconstruction"
+    )
+    print(res)
+
+
+def main() -> int:
+    args = GithubActionsDeployArgs.get_arguments()
+    ssh_client: paramiko.SSHClient = SshUtils.get_ssh_client(
+        server=args.server_address,
+        username=args.username,
+        ssh_key_file=args.ssh_key_file,
+    )
+    run_deployment(ssh_client)
+    ssh_client.close()
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
